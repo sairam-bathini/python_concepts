f54d160ef2558cebc793993ad8f857ac3cebd49e
sairam-bathini <146484884+sairam-bathini@users.noreply.github.com>
2025-01-30 14:41:33 +0530

    Create list_example.py

---
+++ list_example.py
@@ -0,0 +1,7 @@
+# The def keyword in Python is used to define a function. It allows you to create a reusable block of code that can be executed when called.
+
+def access_elements(my_list):
+  print(my_list[0])
+
+example_list = [10, 20, 30, 40)
+access_elements(example_list)
