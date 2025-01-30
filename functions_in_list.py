4d72c6e3484c2d9ba4d9d73c1e3b7c9f02113c3a
sairam-bathini <146484884+sairam-bathini@users.noreply.github.com>
2025-01-30 15:24:30 +0530

    Create functions_in_list.py

---
+++ functions_in_list.py
@@ -0,0 +1,20 @@
+'''Create a list
+append an element in the list
+insert an element in the list
+remove an element in the list
+accessing element from list using indexing
+'''
+# Creating a list
+movie_recommendations_list = ['sye','badra','chirutha','dhruva','eega']
+
+# Add a new element at the end
+movie_recommendations_list.append('f2')
+
+# Inserting an element at a specific position
+movie_recommendations_list.insert(1, 'agni')
+
+# Removing a particular element
+movie_recommendations_list.remove('eega')
+
+# Accessing elements using indexing
+print(movie_recommendations_list[1])
