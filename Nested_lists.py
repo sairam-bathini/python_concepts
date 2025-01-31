b17cd439299645fe469ff6a9990e505c754ba0d5
sairam-bathini <146484884+sairam-bathini@users.noreply.github.com>
2025-01-31 16:48:46 +0530

    Create Nested_lists.py

---
+++ Nested_lists.py
@@ -0,0 +1,77 @@
+'''
+Given the names and grades for each student in a class of  students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+
+Note: If there are multiple students with the second lowest grade, order their names alphabetically and print each name on a new line.
+
+Example
+
+The ordered list of scores is , so the second lowest score is . There are two students with that score: . Ordered alphabetically, the names are printed as:
+
+alpha
+beta
+Input Format
+
+The first line contains an integer, , the number of students.
+The  subsequent lines describe each student over  lines.
+- The first line contains a student's name.
+- The second line contains their grade.
+
+Constraints
+
+There will always be one or more students having the second lowest grade.
+Output Format
+
+Print the name(s) of any student(s) having the second lowest grade in. If there are multiple students, order their names alphabetically and print each one on a new line.
+
+Sample Input 0
+
+5
+Harry
+37.21
+Berry
+37.21
+Tina
+37.2
+Akriti
+41
+Harsh
+39
+Sample Output 0
+
+Berry
+Harry
+Explanation 0
+
+There are  students in this class whose names and grades are assembled to build the following list:
+
+python students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+
+The lowest grade of  belongs to Tina. The second lowest grade of  belongs to both Harry and Berry, so we order their names alphabetically and print each name on a new line.
+'''
+N = int(input())
+students = []
+grades = []
+lowest_grade_names = []
+
+for i in range(N):
+    name = input()
+    grade  = float(input())
+    students.append([name, grade])
+    
+# print(students)
+
+for i in range(N):
+    grades.append(students[i][1])
+
+grades.sort()
+unique_grades = list(set(grades))
+
+for m in range(N):
+    if students[m][1] == unique_grades[1]:
+        lowest_grade_names.append(students[m][0])
+
+lowest_grade_names.sort()
+M = len(lowest_grade_names)
+
+for i in range(M):
+    print(lowest_grade_names[i])
