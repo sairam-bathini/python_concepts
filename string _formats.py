256c27bfacda1bdac5484a0b8f4cc66b10203b4f
sairam-bathini <146484884+sairam-bathini@users.noreply.github.com>
2025-02-03 18:46:33 +0530

    Create string _formats.py

---
+++ string _formats.py
@@ -0,0 +1,27 @@
+
+print("This is a backslash: \\")    # Output: This is a backslash: \
+
+print("This is a single quote: \'") # Output: This is a single quote: '
+
+print("This is a double quote: \"") # Output: This is a double quote: "
+
+print("This is a newline:\nNext line starts here.") 
+# Output: This is a newline:
+#         Next line starts here.
+
+print("This is a tab:\tTabbed space.") 
+# Output: This is a tab:    Tabbed space.
+
+print("This is a carriage return:\rOverwritten text.") 
+# Output: Overwritten text.
+
+print("This is a backspace: Hello\b World") 
+# Output: This is a backspace: Hell World
+
+print("This is a form feed:\fNew page-like effect.") 
+# Output: This is a form feed:
+#         New page-like effect.
+
+print("This is a vertical tab:\vVertical space.") 
+# Output: This is a vertical tab:
+#         Vertical space.
