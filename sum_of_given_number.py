809785d8d3c9a40a9575353a4741c7a9e41ecc37
sairam-bathini <146484884+sairam-bathini@users.noreply.github.com>
2025-02-10 14:23:11 +0530

    Create sum_of_given_number.py

---
+++ sum_of_given_number.py
@@ -0,0 +1,30 @@
+'''
+You are given a two-digit integer n. Return the sum of its digits.
+
+Example
+
+For n = 29, the output should be
+solution(n) = 11.
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[memory limit] 1 GB
+
+[input] integer n
+
+A positive two-digit integer.
+
+Guaranteed constraints:
+10 ≤ n ≤ 99.
+
+[output] integer
+
+The sum of the first and second digits of the input number.
+'''
+
+def solution(n):
+    x = math.floor(n/10)
+    y = n%10
+    return x + y
